fe135c5cab9e7aaa21ae970e8abbaecb36e63c7f
Pankaj Patel <p.patel@holidaypirates.com>
2017-06-15 05:31:47 +0200

    basic loader functionality

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var hyper = require('hyperhtml');
-var _ = require('lodash');
 var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
   this.cacheable && this.cacheable();
   var query = loaderUtils.parseQuery(this.query);
-  var options = this.options.ejsLoader || {};
-
-  ['escape', 'interpolate', 'evaluate'].forEach(function(templateSetting) {
-    var setting = query[templateSetting];
-    if (_.isString(setting)) {
-      query[templateSetting] = new RegExp(setting, 'g');
-    }
-  });
-
-  var template = _.template(source, _.extend({}, query, options));
+  // TODO: Implement possibilities of loader options
+  // var options = this.options.hyperHtmlLoader || {};
+  var template = function (scope) {
+    var render = hyperHTML.wire();
+    return render`
+      ${eval("`"+source+"`")}
+    `;
+  }
   return 'module.exports = ' + template;
 };
